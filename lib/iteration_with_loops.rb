cba8f1158ff84663279e7d7b5ff8e842cacd7e45
Carly L <carlylynnla@gmail.com>
2020-05-06 22:23:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,8 +9,8 @@ def find_even_values(src)
       if src[row_index][element_index]%2 == 0 
         p src[row_index][element_index]
       end
-    end
     element_index += 1
-  end
+    end
   row_index += 1
+  end
 end
